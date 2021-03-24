3cec1debd1bded02b075d663b7f16fb57acd1209
Tamadoc <teri.sandstedt@gmail.com>
2021-03-24 09:10:45 +0100

    Initial commit: empty project with dependencies

---
+++ src/main/java/se/lexicon/teri/mvc_thymeleaf_workshop/MvcThymeleafWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.teri.mvc_thymeleaf_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThymeleafWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcThymeleafWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/teri/mvc_thymeleaf_workshop/MvcThymeleafWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.teri.mvc_thymeleaf_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
