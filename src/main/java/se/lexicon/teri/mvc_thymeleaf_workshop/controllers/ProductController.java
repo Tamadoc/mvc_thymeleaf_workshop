d1396b020cc59613fcff16ff5028d942cf3cc35e
Tamadoc <teri.sandstedt@gmail.com>
2021-03-24 12:44:15 +0100

    Working links

--- src/main/java/se/lexicon/teri/mvc_thymeleaf_workshop/controllers/ProductController.java
+++ src/main/java/se/lexicon/teri/mvc_thymeleaf_workshop/controllers/ProductController.java
@@ -1,18 +1,18 @@
-package se.lexicon.teri.mvc_thymeleaf_workshop.controllers;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import se.lexicon.teri.mvc_thymeleaf_workshop.dto.ProductDto;
-
-import java.util.List;
-
-public class ProductController {
-
-    private List<ProductDto> productDto;
-
-    @GetMapping("/list")
-    public String getAllProducts(Model model){
-        model.addAttribute("productDto", productDto);
-        return "productList";
-    }
-}
+//package se.lexicon.teri.mvc_thymeleaf_workshop.controllers;
+//
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import se.lexicon.teri.mvc_thymeleaf_workshop.dto.ProductDto;
+//
+//import java.util.List;
+//
+//public class ProductController {
+//
+//    private List<ProductDto> productDto;
+//
+//    @GetMapping("/list")
+//    public String getAllProducts(Model model){
+//        model.addAttribute("productDto", productDto);
+//        return "productList";
+//    }
+//}
--- src/main/java/se/lexicon/teri/mvc_thymeleaf_workshop/controllers/ProductManagementController.java
+++ src/main/java/se/lexicon/teri/mvc_thymeleaf_workshop/controllers/ProductManagementController.java
@@ -1,13 +1,17 @@
 package se.lexicon.teri.mvc_thymeleaf_workshop.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.lexicon.teri.mvc_thymeleaf_workshop.dto.ProductDetailsDto;
 import se.lexicon.teri.mvc_thymeleaf_workshop.dto.ProductDto;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 @RequestMapping("/admin/product")
 public class ProductManagementController {
 
@@ -15,9 +19,36 @@ public class ProductManagementController {
 
     @PostConstruct
     public void init() {
-        // instantiate array list
-        // instantiate ProductDto object
-        // add ProductDto to array list
+        if (productDtoList == null) productDtoList= new ArrayList<>();
+
+        // Product 1
+        ProductDetailsDto productDetailsDto= new ProductDetailsDto();
+        productDetailsDto.setId(1);
+        productDetailsDto.setName("Test");
+        productDetailsDto.setDescription("Test Description");
+
+        ProductDto productDto= new ProductDto();
+        productDto.setId(1);
+        productDto.setName("Test Product 1");
+        productDto.setPrice(10);
+        productDto.setProductDetailsDto(productDetailsDto);
+
+
+        // Product 2
+        ProductDetailsDto productDetailsDto2= new ProductDetailsDto();
+        productDetailsDto2.setId(1);
+        productDetailsDto2.setName("Test2");
+        productDetailsDto2.setDescription("Test Description 2");
+
+        ProductDto productDto2= new ProductDto();
+        productDto2.setId(2);
+        productDto2.setName("Test Product 2");
+        productDto2.setPrice(20);
+        productDto2.setProductDetailsDto(productDetailsDto2);
+
+
+        productDtoList.add(productDto);
+        productDtoList.add(productDto2);
     }
 
     @GetMapping("/")
